aa423d4b6282a7d1bba976f10eebe0d84475a32a
Nathan Wenneker <nathan@preferral.com>
2015-11-06 16:59:56 -0700

    Use devtools

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './containers/App'
-import todoApp from './reducers'
+import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
+import configureStore from './store/configureStore'
 
-let store = createStore(todoApp)
+
+let store = configureStore()
 
 let rootElement = document.getElementById('root')
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   rootElement
 )
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,8 +1,12 @@
-import { createStore } from 'redux'
+import { compose, createStore } from 'redux';
 import rootReducer from '../reducers'
+import { devTools } from 'redux-devtools';
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+
+  const finalCreateStore = compose(devTools())(createStore);
+
+  const store = finalCreateStore(rootReducer, initialState)
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
